11b6463f410540f352c34fa45254ca76cb8c1ed8
giulianopa <giuliano.pasqualotto@gmail.com>
2019-09-12 10:48:42 -0700

    Using flock

--- cpp/hw/eeprom.c
+++ cpp/hw/eeprom.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <fcntl.h>
 #include "eeprom.h"
 
 // Check EEPROM offset
@@ -19,6 +21,27 @@
 // EEPROM memory buffer
 static uint8_t eeprom[EEPROM_SZ];
 
+// Lock to protect the buffer
+static pthread_mutex_t buf_mutex;
+
+/**
+ Lock the entire file.
+ Blocker version.
+ \param[in] fd File descriptor, previously opened.
+ \param[in] type l_type value for flock (e.g., F_RDLCK or F_WRLCK).
+ \return True on success, false otherwise.
+ */
+static bool lock_fd(FILE *fd, const unsigned short type) {
+	if (!fd)
+		return false;
+	int fno = fileno(fd);
+	struct flock fl;
+	memset(&fl, 0, sizeof(fl));
+	fl.l_type = type;
+	fl.l_whence = SEEK_SET;
+	return (fcntl(fno, F_SETLKW, &fl) != -1);
+}
+
 // Format: <page_id> <byte_0><byte_1>...<byte_$(EEPROM_PAGE_SZ - 1)>
 uint32_t eeprom_from_file(const char *file_name) {
 	if (!eeprom_erase())
@@ -29,9 +52,14 @@ uint32_t eeprom_from_file(const char *file_name) {
 	fp = fopen(file_name, "r");
 	if (!fp)
 		return 0;
+	if (!lock_fd(fp, F_RDLCK)) {
+		fclose(fp);
+		return 0;
+	}
 	char * line = NULL;
   size_t len = 0;
   ssize_t read = 0;
+	pthread_mutex_lock(&buf_mutex);
 	while (getline(&line, &len, fp) != -1) {
 		char *token = strtok(line, " ,");
 		if (!token)
@@ -49,15 +77,20 @@ uint32_t eeprom_from_file(const char *file_name) {
 			read++;
 		}
 	}
+	pthread_mutex_unlock(&buf_mutex);
 	fclose(fp);
 	return read;
 }
 // Format: <page_id> <byte_0><byte_1>...<byte_$(EEPROM_PAGE_SZ - 1)>
 uint32_t eeprom_to_file(const char *file_name) {
 	FILE *fp = NULL;
-	fp = fopen(file_name, "w");
+	fp = fopen(file_name, "w+");
 	if (!fp)
 		return 0;
+	if (!lock_fd(fp, F_WRLCK)) {
+		fclose(fp);
+		return 0;
+	}
 
 	// Write one page per line
 	uint32_t i = 0, s = 0, written = 0;
@@ -78,36 +111,48 @@ uint32_t eeprom_to_file(const char *file_name) {
 
 uint32_t eeprom_read_word(const uint32_t offset, uint8_t *data) {
 	CHECK_WORD_ADDR(offset);
+	pthread_mutex_lock(&buf_mutex);
 	memcpy(data, eeprom + (EEPROM_WORD_SZ * offset), EEPROM_WORD_SZ);
+	pthread_mutex_unlock(&buf_mutex);
 	return EEPROM_WORD_SZ;
 }
 
 uint32_t eeprom_write_word(const uint32_t offset, const uint8_t *data) {
 	CHECK_WORD_ADDR(offset);
+	pthread_mutex_lock(&buf_mutex);
 	memcpy(eeprom + (EEPROM_WORD_SZ * offset), data, EEPROM_WORD_SZ);
+	pthread_mutex_unlock(&buf_mutex);
 	return EEPROM_WORD_SZ;
 }
 
 uint32_t eeprom_read_page(const uint32_t offset, uint8_t *data) {
 	CHECK_PAGE_ADDR(offset);
+	pthread_mutex_lock(&buf_mutex);
 	memcpy(data, eeprom + (EEPROM_PAGE_SZ * offset), EEPROM_PAGE_SZ);
+	pthread_mutex_unlock(&buf_mutex);
 	return EEPROM_PAGE_SZ;
 }
 
 uint32_t eeprom_write_page(const uint32_t offset, const uint8_t *data) {
 	CHECK_PAGE_ADDR(offset);
+	pthread_mutex_lock(&buf_mutex);
 	memcpy(eeprom + (EEPROM_PAGE_SZ * offset), data, EEPROM_PAGE_SZ);
+	pthread_mutex_unlock(&buf_mutex);
 	return EEPROM_PAGE_SZ;
 }
 
 uint32_t eeprom_erase(void) {
+	pthread_mutex_lock(&buf_mutex);
 	memset(eeprom, EEPROM_ERASE_STATE, EEPROM_SZ);
+	pthread_mutex_unlock(&buf_mutex);
 	return EEPROM_SZ;
 }
 
 uint32_t eeprom_erase_page(const uint32_t offset) {
 	CHECK_PAGE_ADDR(offset);
+	pthread_mutex_lock(&buf_mutex);
 	memset(eeprom + (EEPROM_PAGE_SZ * offset), EEPROM_ERASE_STATE, EEPROM_PAGE_SZ);
+	pthread_mutex_unlock(&buf_mutex);
 	return EEPROM_PAGE_SZ;
 }
 
@@ -115,14 +160,18 @@ uint32_t eeprom_erase_page(const uint32_t offset) {
 bool eeprom_read_byte(const uint32_t offset, uint8_t *data) {
 	if (offset >= EEPROM_SZ)
 		return false;
+	pthread_mutex_lock(&buf_mutex);
 	*data = eeprom[offset];
+	pthread_mutex_unlock(&buf_mutex);
 	return true;
 }
 
 bool eeprom_write_byte(const uint32_t offset, const uint8_t data) {
 	if (offset >= EEPROM_SZ)
 		return false;
+	pthread_mutex_lock(&buf_mutex);
 	eeprom[offset] = data;
+	pthread_mutex_unlock(&buf_mutex);
 	return true;
 }
 #endif
